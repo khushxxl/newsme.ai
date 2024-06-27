83214b14d0ae784fa6b07442bd2fbaa4cb419908
Khushaal Choithramani <khushaal.choithramani@gmail.com>
2024-06-27 23:00:53 +0530

    connect db error fix 01

--- mongodb.ts
+++ mongodb.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDatabase = async () => {
+export const connectDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(
+      "mongodb+srv://khushaal:WUx7GoZ6jfhe9cj0@cluster0.shvhydn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
   }
 };
-
-export default connectDatabase;
